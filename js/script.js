a21760afb0ce8103e9423c7e3aa996cc093d7f82
Nick Hayward <nhayward2011@gmail.com>
2018-02-25 22:01:34 -0500

    making pages load individually and adding a close button

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 
     $('nav a').click(function(e) {
         history.pushState(null, null, $(this).attr('data-content'));
-        $('#content').css('display', 'block');
-        $('#content').load($(this).attr('data-content'));
+        $('#content-container').css('display', 'block');
+        $('#content').load('/' + $(this).attr('data-content') + '/index.html #content >');
+    });
+
+    $('#content-container #close').click(function(e) {
+        history.pushState(null, null, '/');
+        $('#content-container').css('display', 'none');
+        $('#content').load('/index.html #content >');
     });
 
 });
